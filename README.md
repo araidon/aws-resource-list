fb3bc82b6c3b8d55829c70846e455cfaf8b351ed
araidon <arai715work@gmail.com>
2024-12-24 18:14:07 +0900

    output分離, zip圧縮

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # AWSのEC2およびEBSリソースを一覧取得し、タブ区切り形式で出力するスクリプト
 
-AWSのcloud shellにて、aws-resource-list.shをアップロード、実行することで取得できます。
+AWS CloudShellでaws-resource-list.shをアップロードして実行することで取得できます。
 
-デフォルトでは、サブスクライブされている全リージョンに対して実行します。
-リージョン名に引数を指定すると、指定したリージョンのみを対象にします。
+デフォルトでは、サブスクライブされている全リージョンで実行します。
+リージョン名を引数に指定すると、そのリージョンのみを対象にします。
 
 取得出来るリソース
 - EC2
@@ -11,3 +11,19 @@ AWSのcloud shellにて、aws-resource-list.shをアップロード、実行す�
 - RDS
 - S3
 
+## 使用方法
+
+### 全リージョンで実行
+```sh
+sh ./aws-resource-list.sh
+```
+
+### 特定のリージョン（例:東京リージョン）で実行
+```sh
+sh ./aws-resource-list.sh ap-northeast-1
+```
+
+### バージョンを表示
+```sh
+sh ./aws-resource-list.sh --version
+```
--- aws_resource_list.sh
+++ aws_resource_list.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # 各種変数
-VERSION="0.7.0" # version
+VERSION="0.7.1" # version
 accountid=($(aws sts get-caller-identity --query 'Account' --output text))
 date=$(TZ=Asia/Tokyo date -d "yesterday" '+%Y-%m-%d')
-outputfilename=resource_list_${accountid}_$(TZ=Asia/Tokyo date '+%Y%m%d_%H%M%S').csv
-temp_ec2file=list_ec2.txt
-temp_ebsfile=list_ebs.txt
-temp_s3file=list_s3.txt
+# outputfilename=resource_list_${accountid}_$(TZ=Asia/Tokyo date '+%Y%m%d_%H%M%S').csv
+outputfolder=resource_list_${accountid}_$(TZ=Asia/Tokyo date '+%Y%m%d_%H%M%S')
+temp_ec2file=${outputfolder}/temp_ec2.txt
+temp_ebsfile=${outputfolder}/temp_ebs.txt
+temp_s3file=${outputfolder}/temp_s3.txt
+output_ec2file=${outputfolder}/list_ec2.csv
+output_ebsfile=${outputfolder}/list_ebs.csv
+output_rdsfile=${outputfolder}/list_rds.csv
+output_s3file=${outputfolder}/list_s3.csv
 
 # versionオプションが指定された場合、バージョン情報を出力して終了
 if [[ "$1" == "--version" ]]; then
@@ -25,7 +30,9 @@ echo "■対象リージョン: ${regions[@]}"
 total_regions=${#regions[@]}
 
 # Outputファイルの初期化
-echo -n > ${outputfilename}
+#echo -n > ${outputfilename}
+# Outputフォルダの作成
+mkdir -p ${outputfolder}
 
 # プログレスバー表示関数
 show_progress() {
@@ -52,58 +59,58 @@ show_progress() {
 }
 
 # EC2
-echo "■EC2 一覧取得中" 
-for ((i=0; i<total_regions; i++)); do
+echo "■EC2 一覧取得中"
+for ((i = 0; i < total_regions; i++)); do
     region=${regions[$i]}
-    show_progress $((i+1)) $total_regions
+    show_progress $((i + 1)) $total_regions
 
-    aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value | [0], State.Name, InstanceType, PlatformDetails, Placement.AvailabilityZone, BlockDeviceMappings[0].Ebs.VolumeId]' --output json --region ${region} | jq -r '.[][] | @tsv' >> ./${temp_ec2file}
+    aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value | [0], State.Name, InstanceType, PlatformDetails, Placement.AvailabilityZone, BlockDeviceMappings[0].Ebs.VolumeId]' --output json --region ${region} | jq -r '.[][] | @tsv' >>./${temp_ec2file}
 
 done
 sort -t $'\t' -k 6 ./${temp_ec2file} -o ./${temp_ec2file}
 echo ""
 
 # EBS
-echo "■EBS 一覧取得中" 
-for ((i=0; i<total_regions; i++)); do
+echo "■EBS 一覧取得中"
+for ((i = 0; i < total_regions; i++)); do
     region=${regions[$i]}
-    show_progress $((i+1)) $total_regions
+    show_progress $((i + 1)) $total_regions
 
-    aws ec2 describe-volumes --query 'Volumes[*].[VolumeId, Size, VolumeType, AvailabilityZone]' --output json --region ${region} | jq -r '.[] | @tsv' >> ./${temp_ebsfile}
+    aws ec2 describe-volumes --query 'Volumes[*].[VolumeId, Size, VolumeType, AvailabilityZone]' --output json --region ${region} | jq -r '.[] | @tsv' >>./${temp_ebsfile}
 done
 
 sort -t $'\t' -k 1 ./${temp_ebsfile} -o ./${temp_ebsfile}
 echo ""
 
-# EC2とEBSをマージ 
-echo "# EC2" >> ./${outputfilename}
-echo "InstancsName,State,InstanceType,PlatformDetails,AvailabilityZone,Size,VolumeType,VolumeId" >> ./${outputfilename}
+# EC2とEBSをマージ
+#echo "# EC2" >>./${outputfilename}
+echo "InstancsName,State,InstanceType,PlatformDetails,AvailabilityZone,Size,VolumeType,VolumeId" >>./${output_ec2file}
 ## 外部結合(左)
-join -t $'\t' -a 1 -1 6 -2 1 ./${temp_ec2file} ./${temp_ebsfile} | awk -F $'\t' '{print $2 "," $3 "," $4 "," $5 "," $6 "," $7 "," $8 "," $1}' >> ./${outputfilename}
-echo ""  >> ./${outputfilename}
+join -t $'\t' -a 1 -1 6 -2 1 ./${temp_ec2file} ./${temp_ebsfile} | awk -F $'\t' '{print $2 "," $3 "," $4 "," $5 "," $6 "," $7 "," $8 "," $1}' >>./${output_ec2file}
+#echo "" >>./${outputfilename}
 
 ## 一致しない行を出力 = 拡張ボリューム
-echo "# Non-root-Volumes of EBS" >> ./${outputfilename}
-echo "VolumeId,Size,VolumeType,AvailabilityZone" >> ./${outputfilename}
-join -t $'\t' -v 2 -1 6 -2 1 ./${temp_ec2file} ./${temp_ebsfile} | awk -F $'\t' '{print $1 "," $2 "," $3 "," $4}' >> ./${outputfilename}
-echo ""  >> ./${outputfilename}
+#echo "# Non-root-Volumes of EBS" >>./${output_ebsfile}
+echo "VolumeId,Size,VolumeType,AvailabilityZone" >>./${output_ebsfile}
+join -t $'\t' -v 2 -1 6 -2 1 ./${temp_ec2file} ./${temp_ebsfile} | awk -F $'\t' '{print $1 "," $2 "," $3 "," $4}' >>./${output_ebsfile}
+#echo "" >>./${output_ebsfile}
 
 # RDS
-echo "■RDS 一覧取得中" 
-echo "# RDS"  >> ./${outputfilename}
-echo "DBInstanceIdentifier, DBInstanceStatus, DBInstanceClass, Engine, EngineVersion, AvailabilityZone, MultiAZ, StorageType, AllocatedStorage" >> ./${outputfilename}
-for ((i=0; i<total_regions; i++)); do
+echo "■RDS 一覧取得中"
+#echo "# RDS" >>./${outputfilename}
+echo "DBInstanceIdentifier, DBInstanceStatus, DBInstanceClass, Engine, EngineVersion, AvailabilityZone, MultiAZ, StorageType, AllocatedStorage" >>./${output_rdsfile}
+for ((i = 0; i < total_regions; i++)); do
     region=${regions[$i]}
-    show_progress $((i+1)) $total_regions
-	aws rds describe-db-instances --query 'DBInstances[*].[DBInstanceIdentifier, DBInstanceStatus, DBInstanceClass, Engine, EngineVersion, AvailabilityZone, MultiAZ, StorageType, AllocatedStorage]' --output text --region ${region} | sed -e 's/\t/,/g' >> ./${outputfilename}
+    show_progress $((i + 1)) $total_regions
+    aws rds describe-db-instances --query 'DBInstances[*].[DBInstanceIdentifier, DBInstanceStatus, DBInstanceClass, Engine, EngineVersion, AvailabilityZone, MultiAZ, StorageType, AllocatedStorage]' --output text --region ${region} | sed -e 's/\t/,/g' >>./${output_rdsfile}
 done
-echo ""  | tee -a ${outputfilename}
+echo ""
 
 # S3
-echo "■S3 一覧取得中" 
-echo "# S3"  >> ./${outputfilename}
-echo "BucketName,Region,Date,Size(Bytes)" >> ./${outputfilename}
-aws s3 ls > ./${temp_s3file}
+echo "■S3 一覧取得中"
+#echo "# S3" >>./${outputfilename}
+echo "BucketName,Region,Date,Size(Bytes)" >>./${output_s3file}
+aws s3 ls >./${temp_s3file}
 total_buckets=$(awk '{print $3}' "./${temp_s3file}" | wc -l)
 current_bucket=0
 
@@ -113,7 +120,7 @@ awk '{print $3}' "./${temp_s3file}" | while read bucket_name; do
     current_bucket=$((current_bucket + 1))
     show_progress $current_bucket $total_buckets
 
-	# S3バケットのリージョン名を取得
+    # S3バケットのリージョン名を取得
     region=$(aws s3api get-bucket-location --bucket "$bucket_name" --query 'LocationConstraint' --output text)
     if [ "$region" == "None" ]; then
         region="ap-northeast-1"
@@ -129,15 +136,15 @@ awk '{print $3}' "./${temp_s3file}" | while read bucket_name; do
         --namespace AWS/S3 \
         --metric-name BucketSizeBytes \
         --dimensions \
-            Name=StorageType,Value=StandardStorage \
-            Name=BucketName,Value="$bucket_name" \
+        Name=StorageType,Value=StandardStorage \
+        Name=BucketName,Value="$bucket_name" \
         --statistics Maximum \
         --start-time "${date}T00:00:00Z" \
         --end-time "${date}T23:59:59Z" \
         --period 86400 \
         --unit Bytes \
-        --region "$region" \
-        | jq -r --arg bucket_name "$bucket_name" --arg region "$region" '
+        --region "$region" |
+        jq -r --arg bucket_name "$bucket_name" --arg region "$region" '
             .Datapoints
             | sort_by(.Timestamp)
             | .[]
@@ -147,15 +154,21 @@ awk '{print $3}' "./${temp_s3file}" | while read bucket_name; do
 
     # メトリクスが空の場合、バケット名とリージョン名だけをCSVに出力
     if [ -z "$metrics" ]; then
-        echo "$bucket_name,$region,," >> "${outputfilename}"
+        echo "$bucket_name,$region,," >>"${output_s3file}"
     else
-        echo "$metrics" >> "${outputfilename}"
+        echo "$metrics" >>"${output_s3file}"
     fi
 done
 echo ""
 
-# 一時ファイルの整理 
-rm ./${temp_ec2file}
-rm ./${temp_ebsfile}
-rm ./${temp_s3file}
+# Clean up temporary files
+#rm ./${temp_ec2file}
+#rm ./${temp_ebsfile}
+#rm ./${temp_s3file}
 
+# Zip the output folder
+zip -r ${outputfolder}.zip ${outputfolder}
+
+echo "Output file: ${outputfolder}.zip"
+echo "Done."
+# End of script
